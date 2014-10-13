e8b382b72611f49ad54e4934eb0d699ce2c89d27
Andrew Hills <ahills60@gmail.com>
2014-10-13 15:50:11 +0100

    Adds scenery loader to tree analyser
    - Tree analyser can now load scenery files to memory.
    - Additional counters added to support the loading of scenery files.
    - Trees are now opened as binary files following tree update in
    ahills60/DBSimulator@e3bdeed98f3e1968cf43d60e88fa36afd95b3086.

--- TreeAnalyser.c
+++ TreeAnalyser.c
@@ -32,6 +32,7 @@ void specialReleaseFunc(int key, int xmouse, int ymouse);
 void mouseFunc(int button, int state, int xmouse, int ymouse);
 void mouseMoveFunc(int xmouse, int ymouse);
 void LoadTree(char *filename);
+int LoadScenery(char *filename);
 
 // Global variables
 int mainWindow, treeSubWindow, sceneSubWindow;
@@ -51,6 +52,9 @@ int noSplitListEntries = 0;
 int noTreeListEntries = 0;
 int noTreeMatrixEntries = 0;
 int noNodeListEntries = 0;
+int noTriangles = 0;
+int noMaterials = 0;
+int noTextures = 0;
 
 
 // Main window display function
@@ -242,11 +246,23 @@ int main (int argc, char *argv[])
         }
     }
     
+    // Check the tree variable for assignment.
     if (!tree)
     {
         // It wasn't assigned to anything.
         printf("ERROR: Tree filename needs to be specified.\n\n");
         exit(-1);
+    }
+    
+    // If here, we can load the tree:
+    LoadTree(treeFilename);
+    
+    // Has the scenery filename been defined?
+    if (sceneFilename)
+    {
+        // Yes, then load the scenery file.
+        if(!LoadScenery(sceneFilename))
+            printf("WARNING: Unable to load the scenery file \"%s\". Verify that it exists.\n\n", sceneFilename);
     }
     
     return 1;
@@ -261,7 +277,7 @@ void LoadTree(char *filename)
     printf("Restoring tree to memory...\n");
     
     // Open the file for reading
-    fp = fopen(filename, "r");
+    fp = fopen(filename, "rb");
     
     if (!fp)
     {
@@ -305,4 +321,234 @@ void LoadTree(char *filename)
     fclose(fp);
     
     printf("Tree state restored from \"%s\".\n\n", filename);
+}
+
+// Function to load the scenery file to memory and populate the object and material database
+int LoadScenery(char *filename)
+{
+    FILE *fp;
+    int x, y, z, a, b;
+    int n, m, zeroCheck, matIdx, textIdx, localNoTriangles, localNoMaterials, localNoTextures;
+    char *textureFilename;
+    int DominantAxisIdx, NUDom, NVDom, NDDom, BUDom, BVDom, CUDom, CVDom, totalTriangles = noTriangles;
+    
+    // File initialisation:
+    printf("Reading world file \"%s\"\n", filename);
+    
+    fp = fopen(filename, "rb");
+    
+    // Populate local counters
+    fread(&localNoMaterials, sizeof(int), 1, fp);
+    fread(&localNoTextures, sizeof(int), 1, fp);
+    
+    // Verify that we're not going to overflow:
+    if (localNoTextures + noTextures > MAX_TEXTURES)
+    {
+        printf("Too many textures requested (%i). Current texture limit set to %i.\n\n", localNoTextures + noTextures, MAX_TEXTURES);
+        fclose(fp);
+        return 0;
+    }
+    if (localNoMaterials + noMaterials > MAX_MATERIALS)
+    {
+        printf("Too many materials requested (%i). Current material limit set to %i.\n\n", localNoMaterials + noMaterials, MAX_MATERIALS);
+        fclose(fp);
+        return 0;
+    }
+    
+    // Now read the textures:
+    for (n = 0; n < localNoTextures; n++)
+    {
+        // Read the filename:
+        fread(&m, sizeof(int), 1, fp);
+        textureFilename = (char *) malloc(sizeof(char) * (m + 1));
+        // Blank memory:
+        memset(textureFilename, 0, sizeof(char) * (m + 1));
+        // Then read the m characters:
+        fread(textureFilename, sizeof(char), m, fp);
+        
+        // Cater for the special case:
+        if (strcmp(textureFilename, "terrain.tga") != 0 && m != 25)
+        {
+            memcpy(textureFilename, textureFilename + 12, 36);
+            memset(textureFilename + 36, 0, sizeof(char) * 12);
+        }
+        else
+        {
+            if (m == 25)
+                textureFilename[8] = '/';
+        }
+        
+        // Read the texture to memory
+        ReadTexture(noTextures + n, textureFilename);
+        
+        // Before finally freeing up the memory:
+        free(textureFilename);
+    }
+    
+    // Increment the global number of textures
+    noTextures += localNoTextures;
+    
+    // We should now encounter a zero in the file. Verify this is the case:
+    fread(&zeroCheck, sizeof(int), 1, fp);
+    if (zeroCheck != 0)
+    {
+        printf("\nERROR: Error encountered entering filenames. Failed zero check.\n");
+        fclose(fp);
+        return 0;
+    }
+    
+    // Now proceed to load materials
+    for (n = 0; n < localNoMaterials; n++)
+    {
+        // Read the material index and then read the texture index:
+        fread(&matIdx, sizeof(int), 1, fp);
+        fread(&textIdx, sizeof(int), 1, fp);
+        
+        // Now create a material:
+        setMaterial(matIdx, textIdx);
+    }
+    
+    // Increment the global number of materials
+    noMaterials == localNoMaterials;
+    
+    // We should now encounter a zero in the file. Verify this is the case:
+    fread(&zeroCheck, sizeof(int), 1, fp);
+    if (zeroCheck != 0)
+    {
+        printf("\nERROR: Error encountered pairing materials with textures. Failed zero check.\n");
+        fclose(fp);
+        return 0;
+    }
+    
+    // Start the triangle loading process.
+    fread(&localNoTriangles, sizeof(int), 1, fp);
+    
+    while(!feof(fp))
+    {
+        // Check to see if we're over the limit:
+        if (localNoTriangles + noTriangles > MAX_TRIANGLES)
+        {
+            printf("Too many triangles requested (%i). Current triangle limit set to %i.\n\n", localNoTriangles + noTriangles, MAX_TRIANGLES);
+            fclose(fp);
+            return 0;
+        }
+        
+        for (n = 0; n < localNoTriangles; n++)
+        {
+            // Start reading the parameters for each triangle
+            
+            // VECTOR A:
+            // Vector coords
+            fread(&x, sizeof(int), 1, fp);
+            fread(&y, sizeof(int), 1, fp);
+            fread(&z, sizeof(int), 1, fp);
+            // UV Coords
+            fread(&a, sizeof(int), 1, fp);
+            fread(&b, sizeof(int), 1, fp);
+            
+            // Add vector and UV coords to the DB
+            ObjectDB[noTriangles][TriangleAx] = (float) x / 65536.0;
+            ObjectDB[noTriangles][TriangleAy] = (float) y / 65536.0;
+            ObjectDB[noTriangles][TriangleAz] = (float) z / 65536.0;
+            ObjectDB[noTriangles][TriangleAu] = (float) a / 65536.0;
+            ObjectDB[noTriangles][TriangleAv] = (float) b / 65536.0;
+            
+            // VECTOR B:
+            // Vector coords
+            fread(&x, sizeof(int), 1, fp);
+            fread(&y, sizeof(int), 1, fp);
+            fread(&z, sizeof(int), 1, fp);
+            // UV Coords
+            fread(&a, sizeof(int), 1, fp);
+            fread(&b, sizeof(int), 1, fp);
+            
+            // Add vector and UV coords to the DB
+            ObjectDB[noTriangles][TriangleBx] = (float) x / 65536.0;
+            ObjectDB[noTriangles][TriangleBy] = (float) y / 65536.0;
+            ObjectDB[noTriangles][TriangleBz] = (float) z / 65536.0;
+            ObjectDB[noTriangles][TriangleBu] = (float) a / 65536.0;
+            ObjectDB[noTriangles][TriangleBv] = (float) b / 65536.0;
+            
+            // VECTOR C:
+            // Vector coords
+            fread(&x, sizeof(int), 1, fp);
+            fread(&y, sizeof(int), 1, fp);
+            fread(&z, sizeof(int), 1, fp);
+            // UV Coords
+            fread(&a, sizeof(int), 1, fp);
+            fread(&b, sizeof(int), 1, fp);
+            
+            // Add vector and UV coords to the DB
+            ObjectDB[noTriangles][TriangleCx] = (float) x / 65536.0;
+            ObjectDB[noTriangles][TriangleCy] = (float) y / 65536.0;
+            ObjectDB[noTriangles][TriangleCz] = (float) z / 65536.0;
+            ObjectDB[noTriangles][TriangleCu] = (float) a / 65536.0;
+            ObjectDB[noTriangles][TriangleCv] = (float) b / 65536.0;
+            
+            // TRIANGLE PARAMETERS:
+            // The dominant axis:
+            fread(&DominantAxisIdx, sizeof(int), 1, fp);
+            ObjectDB[noTriangles][TriangleDominantAxisIdx] = (float) DominantAxisIdx / 65536.0;
+            
+            // vmu, wmu, normdom:
+            fseek(fp, 9 * sizeof(int), SEEK_CUR);
+            
+            // VECTOR normcrvmuwmu:
+            // Vector coords
+            fread(&x, sizeof(int), 1, fp);
+            fread(&y, sizeof(int), 1, fp);
+            fread(&z, sizeof(int), 1, fp);
+            
+            // Add vector and UV coords to the DB
+            ObjectDB[noTriangles][Trianglenormcrvmuwmux] = (float) x / 65536.0;
+            ObjectDB[noTriangles][Trianglenormcrvmuwmuy] = (float) y / 65536.0;
+            ObjectDB[noTriangles][Trianglenormcrvmuwmuz] = (float) z / 65536.0;
+            
+            fread(&NUDom, sizeof(int), 1, fp);
+            fread(&NVDom, sizeof(int), 1, fp);
+            fread(&NDDom, sizeof(int), 1, fp);
+            fread(&BUDom, sizeof(int), 1, fp);
+            fread(&BVDom, sizeof(int), 1, fp);
+            fread(&CUDom, sizeof(int), 1, fp);
+            fread(&CVDom, sizeof(int), 1, fp);
+            
+            ObjectDB[noTriangles][TriangleNUDom] = (float) NUDom / 65536.0;
+            ObjectDB[noTriangles][TriangleNVDom] = (float) NVDom / 65536.0;
+            ObjectDB[noTriangles][TriangleNDDom] = (float) NDDom / 65536.0;
+            ObjectDB[noTriangles][TriangleBUDom] = (float) BUDom / 65536.0;
+            ObjectDB[noTriangles][TriangleBVDom] = (float) BVDom / 65536.0;
+            ObjectDB[noTriangles][TriangleCUDom] = (float) CUDom / 65536.0;
+            ObjectDB[noTriangles][TriangleCVDom] = (float) CVDom / 65536.0;
+            
+            noTriangles++;
+        }
+        
+        // Next, read the material index for this batch of triangles:
+        fread(&matIdx, sizeof(int), 1, fp);
+        
+        // Triangles are now added. Assign material index to the triangles:
+        for (n = totalTriangles; n < noTriangles; n++)
+            ObjectDB[n][TriangleMaterialIdx] = (float) matIdx / 65536.0;
+        
+        // Then add this batch to the list:
+        totalTriangles += localNoTriangles;
+        
+        // This is followed by a zero check:
+        fread(&zeroCheck, sizeof(int), 1, fp);
+        if (zeroCheck != 0)
+        {
+            printf("\nERROR: Error encountered pairing triangle points with UV values. Failed zero check.\n");
+            fclose(fp);
+            return 0;
+        }
+        // Now read the next number of triangle values. EOF will raise if this failed.
+        fread(&localNoTriangles, sizeof(int), 1, fp);
+    }
+    
+    // File read.
+    printf("Scenery file read successfully.\n\n");
+    
+    // Close the file pointer before terminating:
+    fclose(fp);
+    return 1;
 }
