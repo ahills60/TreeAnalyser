e302eac439bbe2473acd28c8e339b413ac592371
Andrew Hills <ahills60@gmail.com>
2014-10-14 16:24:52 +0100

    Fixes colour definition in tree analyser header
    - Colour definition in tree analyser header file was incorrectly
    changed. This has now been corrected.

--- TreeAnalyser.h
+++ TreeAnalyser.h
@@ -25,7 +25,7 @@
 #define NODE_DRAW_SQUARE_SIZE                   5.0
 #define NODE_DRAW_SQUARE_COLOUR_R               1.0
 #define NODE_DRAW_SQUARE_COLOUR_G               0.0
-#define NODE_DRAW_LINE_COLOUR_B                 0.0
+#define NODE_DRAW_SQUARE_COLOUR_B               0.0
 #define NODE_DRAW_LINE_COLOUR_R                 0.0
 #define NODE_DRAW_LINE_COLOUR_G                 1.0
 #define NODE_DRAW_LINE_COLOUR_B                 0.0
